59aeddf26fd8c4532eb0eff5c9b4a8101e626dbf
beans-are-gross <94317146+beans-are-gross@users.noreply.github.com>
2026-01-17 16:32:00 -0600

    Add loopDir variable

--- start_slideshow.sh
+++ start_slideshow.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+loopDir="<your folder>"   # Edit this line only.
+
 sleep 5                   # Wait for the desktop to be ready.
 
 xset s off                # Disable screen saver timer
@@ -7,10 +9,10 @@ xset -dpms                # Disable Power Management (the 10-minute shutdown)
 xset s noblank            # Prevent the screen from going black
 
 # Deletes Mac metadata, Windows thumbnails, and Linux thumbnails
-find <your folder> -name "._*" -delete -o -name "thumbs.db" -delete -o -name "desktop.ini" -delete
+find "$loopDir" \( -name "._*" -o -name "thumbs.db" -o -name "desktop.ini" \) -delete
 
 while true; do            # Loop forever just incase a file in the loop isnt an image file.
-  feh -F -Z -Y -z -D 10 --auto-rotate --reload 60 --quiet --recursive /home/pi/Pictures/
+  feh -F -Z -Y -z -D 10 --auto-rotate --reload 60 --quiet --recursive $loopDir
 
   if [ $? -eq 0 ]; then   # Check if the esc key was pressed and end the loop.
     break
